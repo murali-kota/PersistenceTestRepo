97c37a3197c95499119b8a58d44abf2d9017e28e
murali-kota <60190214+murali-kota@users.noreply.github.com>
2022-08-05 00:49:44 +0100

    first time checkin
    first time checkin

---
+++ Language_Translate/Language_Translate/Program.cs
@@ -0,0 +1,76 @@
+﻿
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Language_Translate
+{
+    public class LanguageTranslator
+    {
+        public static ILoggerFactory LoggerFactory;
+        public static IConfigurationRoot Configuration;
+        public static void Main(string[] args)
+        {
+            LanguageTranslator languageTranslator = new LanguageTranslator();
+            string textToTranslate = string.Empty;
+            while(textToTranslate.ToLower() != "exit")
+            {
+                textToTranslate= Console.ReadLine();
+                languageTranslator.TranslateTextRequest("en", "to", textToTranslate);
+                
+            }//while
+
+        }
+        public LanguageTranslator()
+        {
+            var builder = new ConfigurationBuilder()
+                                            .SetBasePath(Directory.GetCurrentDirectory())
+                                            .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+        }
+        public async Task<string> TranslateTextRequest(string from, string to,  string inputText)
+        {
+            string resourceKey = Configuration["TranslatorKey"];
+            string endpoint = Configuration["TextTranslatorAPI"];
+            string region = Configuration["region"];
+            
+
+            object[] body = new object[] { new { Text = inputText } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                // Build the request.
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(string.Format(endpoint, from, to));
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", resourceKey);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", region);
+
+                // Send the request and get response.
+                HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+                // Read response as a string.
+                string result = await response.Content.ReadAsStringAsync();
+                TranslationResult[] deserializedOutput = JsonConvert.DeserializeObject<TranslationResult[]>(result);
+                // Iterate over the deserialized results.
+                //foreach (TranslationResult o in deserializedOutput)
+                //{
+                // Print the detected input language and confidence score.
+                //Console.WriteLine("Detected input language: {0}\nConfidence score: {1}\n", o.DetectedLanguage.Language, o.DetectedLanguage.Score);
+                // Iterate over the results and print each translation.
+                //foreach (Translation t in o.Translations)
+                //{
+                //    Console.WriteLine("Translated to {0}: {1}", t.To, t.Text);
+                //}                   
+                //}
+                return deserializedOutput == null?null: deserializedOutput[0].Translations[0].Text;
+                
+            }
+        }
+    }
+}
---
+++ Language_Translate/Language_Translate_Tests/UnitTest1.cs
@@ -0,0 +1,57 @@
+using Language_Translate;
+namespace Language_Translate_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test_for_sample_case()
+        {
+            LanguageTranslator languageTranslator = new LanguageTranslator();
+            string translatedText = languageTranslator.TranslateTextRequest("en", "fr", "test words").Result;
+            Assert.IsNotNull(translatedText);
+            Assert.IsNotEmpty(translatedText);
+            Assert.That(translatedText, Is.EqualTo("mots d'essai"));
+            Assert.Pass();
+        }
+        public void Test_for_first_sentence_given()
+        {
+            LanguageTranslator languageTranslator = new LanguageTranslator();
+            string translatedText = languageTranslator.TranslateTextRequest("en", "fr", @"Farnborough International Airshow, biennial global aerospace, defence and space trade event 
+                                                                                            which showcases the latest commercial and military aircraft.Manufacturers such as Airbus and Boeing
+                                                                                            are expected to display their products and announce new orders* 2020 event was held virtually after
+                                                                                            the physical show was cancelled due to the coronavirus(COVID-19) pandemic").Result;
+            Assert.IsNotNull(translatedText);
+            Assert.IsNotEmpty(translatedText);
+            Assert.That(translatedText, Is.EqualTo("Farnborough International Airshow, événement mondial biennal sur le commerce de l’aérospatiale, de la défense et de l’espace \r\nqui présente les derniers avions commerciaux et militaires. Des constructeurs comme Airbus et Boeing \r\ndevraient exposer leurs produits et annoncer les nouvelles commandes * L’événement 2020 a eu lieu virtuellement après \r\nle spectacle physique a été annulé en raison de la pandémie de coronavirus (COVID-19)"));
+            Assert.Pass();
+        }
+
+        public void Test_for_second_sentence_given()
+        {
+            LanguageTranslator languageTranslator = new LanguageTranslator();
+            string translatedText = languageTranslator.TranslateTextRequest("en", "fr", @"Labour market statistics: integrated national release, including the latest data for employment, 
+                                                                                        economic activity, economic inactivity, unemployment, claimant count, average earnings, productivity, 
+                                                                                        unit wage costs, vacancies & labour disputes").Result;
+            Assert.IsNotNull(translatedText);
+            Assert.IsNotEmpty(translatedText);
+            Assert.That(translatedText, Is.EqualTo(@"Statistiques du marché du travail : diffusion nationale intégrée, y compris les données les plus récentes sur l’emploi, l’activité économique, l’inactivité économique, le chômage, le nombre de prestataires, les gains moyens, la productivité, les coûts salariaux unitaires, les postes vacants et les conflits du travail"));
+            Assert.Pass();
+        }
+        public void Test_for_third_sentence_given()
+        {
+            LanguageTranslator languageTranslator = new LanguageTranslator();
+            string translatedText = languageTranslator.TranslateTextRequest("en", "fr", @"City of London Corporation's Financial and Professional Services dinner. Chancellor Rishi Sunak 
+                                                                                            and Bank of England Governor Andrew Bailey make their annual Mansion House speeches at the event 
+                                                                                            hosted by the Lord Mayor of the City of London Vincent Keaveny").Result;
+            Assert.IsNotNull(translatedText);
+            Assert.IsNotEmpty(translatedText);
+            Assert.That(translatedText, Is.EqualTo("Dîner sur les services financiers et professionnels de la City of London Corporation. Le chancelier Rishi Sunak et le gouverneur de la Banque d’Angleterre Andrew Bailey prononcent leurs discours annuels à Mansion House lors de l’événement organisé par le maire de la ville de Londres, Vincent Keaveny."));
+            Assert.Pass();
+        }
+    }
+}
